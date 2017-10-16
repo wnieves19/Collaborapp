52c27dfed0a602bbded50ee6e10dc0548e933c59
Wilfredo Nieves <wnieves@optivon.net>
2017-10-16 09:26:21 -0400

    Redirect to ChatListActivity after login

---
+++ app/src/main/java/io/collaborapp/collaborapp/chat/ChatListActivity.java
@@ -0,0 +1,30 @@
+package io.collaborapp.collaborapp.chat;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import io.collaborapp.collaborapp.R;
+
+public class ChatListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+}
--- app/src/main/java/io/collaborapp/collaborapp/login/LoginFragment.java
+++ app/src/main/java/io/collaborapp/collaborapp/login/LoginFragment.java
@@ -1,14 +1,17 @@
 package io.collaborapp.collaborapp.login;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -25,6 +28,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 import io.collaborapp.collaborapp.R;
+import io.collaborapp.collaborapp.chat.ChatListActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -37,7 +41,6 @@ public class LoginFragment extends Fragment implements GoogleApiClient.OnConnect
     public LoginFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -95,10 +98,18 @@ public class LoginFragment extends Fragment implements GoogleApiClient.OnConnect
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(getActivity(), ChatListActivity.class));
                         } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("Collaborapp", "signInWithCredential:failure", task.getException());
+                            AlertDialog alertDialog = new AlertDialog.Builder(getActivity()).create();
+                            alertDialog.setTitle("Alert");
+                            alertDialog.setMessage("Could not login, try later");
+                            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.dismiss();
+                                        }
+                                    });
+                            alertDialog.show();
                         }
                     }
                 });
@@ -106,6 +117,6 @@ public class LoginFragment extends Fragment implements GoogleApiClient.OnConnect
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        Toast.makeText(getActivity(),  "Could not login, try again.", Toast.LENGTH_SHORT).show();
     }
 }
