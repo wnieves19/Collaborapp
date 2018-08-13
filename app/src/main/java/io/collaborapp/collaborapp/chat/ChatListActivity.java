9a628738858823303a7c04a280fb0e09db353b8e
Wilfredo Nieves <wnieves@optivon.net>
2018-08-12 23:18:51 -0400

    Add Menu to ChatListAcrivity

--- app/src/main/java/io/collaborapp/collaborapp/chat/ChatListActivity.java
+++ app/src/main/java/io/collaborapp/collaborapp/chat/ChatListActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import io.collaborapp.collaborapp.R;
@@ -14,7 +17,7 @@ public class ChatListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -26,4 +29,20 @@ public class ChatListActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.sign_out:{
+                //TODO: Sign out
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
